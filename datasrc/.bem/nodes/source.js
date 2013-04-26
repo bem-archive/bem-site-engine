3ee3b6efccfd53760ca73c88ceb9a209575790db
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-04-26 19:21:03 +0400

    Собираем интроспекцию библиотек при сборке

--- datasrc/.bem/nodes/source.js
+++ datasrc/.bem/nodes/source.js
@@ -2,12 +2,15 @@ var PATH = require('path'),
     BEM = require('bem'),
     Q = BEM.require('q'),
     registry = BEM.require('./nodesregistry.js'),
+    nodes = BEM.require('./nodes/node.js'),
     cacheNodes = require('./cache.js'),
+    introspectorNodes = require('./introspector.js'),
 
     PRJ_ROOT = PATH.resolve(__dirname, '../../../'),
     environ = require(PATH.join(PRJ_ROOT, '.bem/environ.js')),
 
-    CACHE_NODEID = exports.CACHE_NODEID = 'cache';
+    CACHE_NODEID = exports.CACHE_NODEID = 'cache',
+    SOURCE_NODEID = exports.SOURCE_NODEID = 'sources';
 
 
 var SourceNodeName = exports.SourceNodeName = 'SourceNode';
@@ -23,11 +26,18 @@ registry.decl(SourceNodeName, {
     },
 
     alterArch : function() {
-        var cacheNode = this.createCacheNode();
+        var cacheNode = this.createCacheNode(),
+            sourceNode = this.createSourcesNode();
 
         return Q.all(this.getLibraries().map(function(id) {
                 var lib = this.getLibCredentials(id);
                 cacheNode.pushToCache(lib);
+
+                var itemNode = new (registry.getNodeClass(SourceItemNodeName))({
+                        root : this.root,
+                        item : lib
+                    });
+                this.arch.setNode(itemNode, sourceNode);
             }, this))
             .then(function() {
                 return this.arch;
@@ -35,11 +45,17 @@ registry.decl(SourceNodeName, {
     },
 
     createCacheNode : function() {
-        var node = new (registry.getNodeClass(cacheNodes.CacheNodeName))({
+        var node = new cacheNodes.CacheNode({
                 id   : CACHE_NODEID,
                 arch : this.arch,
                 root : this.root
             });
+        this.arch.setNode(node);
+        return node;
+    },
+
+    createSourcesNode : function() {
+        var node = new nodes.Node(SOURCE_NODEID);
         this.arch.setNode(node);
         return node;
     },
@@ -60,6 +76,67 @@ registry.decl(SourceNodeName, {
         credential._bemDeps = true;
 
         return credential;
+    }
+
+});
+
+
+var SourceItemNodeName = exports.SourceItemNodeName = 'SourceItemNode';
+Object.defineProperty(exports, SourceItemNodeName, {
+    get : function() { return registry.getNodeClass(SourceItemNodeName) }
+});
+
+registry.decl(SourceItemNodeName, nodes.NodeName, {
+
+    __constructor : function(o) {
+        this.__base(o);
+        this.root = o.root;
+        this.item = o.item;
+        this.path = '_' + o.item.id;
+    },
+
+    getPath : function() {
+        return PATH.join(this.root, this.path);
+    },
+
+    make : function() {
+        return this.ctx.arch.withLock(this.createIntrospectorNode(), this);
+    },
+
+    createIntrospectorNode : function() {
+        var ctx = this.ctx;
+        return function() {
+            var arch = ctx.arch,
+                lib = this.item,
+                cacheItemNode = ctx.arch.getNode(lib._id + '*'),    // FIXME: hardcode
+                spectrNode = new introspectorNodes.IntrospectorNode({
+                    id : lib._id + '-spectr',
+                    root : cacheItemNode.getPath(),
+                    sources : ['common.blocks']     // FIXME: hardcode
+                }),
+                realSINode = new nodes.Node(this.path);
+
+            arch.setNode(realSINode, arch.getParents(this));
+
+            return spectrNode.getStruct()
+                .then(function(decl) {
+                    var spectrItemNode = new introspectorNodes.IntrospectorItemNode({
+                        root : this.root,
+                        path : lib._id,
+                        decl : decl
+                    });
+
+                    arch.setNode(spectrItemNode, realSINode, cacheItemNode.getId());
+
+                    return spectrItemNode;
+                }.bind(this));
+        };
+    }
+
+}, {
+
+    createId : function(o) {
+        return '_' + o.item._id + '*';
     }
 
 });
