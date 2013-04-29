e5940675eb0f981e5bf0f36b33af2b01407299ab
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-04-29 17:14:31 +0400

    Удалил рудименты

--- datasrc/.bem/sources.js
+++ datasrc/.bem/sources.js
@@ -23,46 +23,11 @@ registry.decl(sourceNodes.SourceNodeName, {
         return [
 //            'islands-controls',
             'islands-popups',
-            'islands-user',
-            'islands-media'
+//            'islands-user',
+//            'islands-media'
         ];
     }
 
 });
 
 // TODO: разобраться с npm-пакетами в библиотеках (Error: Cannot find module 'dom-js')
-// TODO: унести в отдельный узел (нужна зависимость spectrItemNode -> outputNodes)
-/*
-registry.decl(sourceNodes.SourceItemNodeName, {
-
-    createIntrospectorNode : function() {
-        return this.__base.apply(this, arguments)
-            .then(function(spectrItemNode) {
-                var outputNodes = require(PATH.join(BGD_ROOT, '.bem/nodes/output.js')),
-                    cacheItemNode = this.ctx.arch.getNode(this.item._id + '*'),
-                    itemNode = new outputNodes.CatalogueItemNode({
-                        root : cacheItemNode.getPath(), // XXX: note `path`!
-                        level : '',
-                        item : { block : 'islands-popups' },
-                        techName : 'data.json'
-                    }),
-                    blockPath = PATH.join(this.getPath(), 'blocks');
-                return QFS.makeTree(blockPath)
-                    .invoke.call(spectrItemNode, 'readDecl')
-                    .then(function(decl) {
-
-                        return Q.all(Object.keys(decl).map(function(block) {
-                            return this.translateMeta(decl[block])
-                                .then(function(data) {
-                                    var path = PATH.join(blockPath, [block, 'json.js'].join('.'));
-                                    return U.writeFileIfDiffers(path, JSON.stringify(data, null, 2));
-                                });
-                        }, this));
-
-                    }.bind(itemNode));
-
-            }.bind(this));
-    }
-
-});
-*/
