77107f5dd4be87b120b55cd618d3c1fa04b53be5
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-04-29 23:10:03 +0400

    Базовые блоки проека

---
+++ desktop.blocks/b-page/b-page.deps.js
@@ -0,0 +1,10 @@
+({
+    mustDeps : [],
+    shouldDeps : [
+        { mods : { theme : 'normal' } },
+        { block : 'header' },
+        { block : 'layout', mods : { type : 'serp' } },
+        { block : 'content' },
+        { block : 'footer' }
+    ]
+})
