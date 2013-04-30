c7485c698e3d027b8b316c4e5a11fba0fa2608f0
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-04-30 15:40:32 +0400

    Добавил библиотек для тестов

--- datasrc/.bem/sources.js
+++ datasrc/.bem/sources.js
@@ -2,9 +2,7 @@ var PATH = require('path'),
     BEM = require('bem'),
     Q = BEM.require('q'),
     QFS = BEM.require('q-fs'),
-
     registry = BEM.require('./nodesregistry.js'),
-
     cacheNodes = require('./nodes/cache.js'),
     libNodes = require('./nodes/lib.js'),
     sourceNodes = require('./nodes/source.js');
@@ -13,18 +11,20 @@ var PATH = require('path'),
     PRJ_ROOT = PATH.resolve(__dirname, '../../');
 
 process.env.__root_level_dir = PRJ_ROOT;
-var environ = require('../../.bem/environ'),
-    BGD_ROOT = environ.getLibPath('bem-gen-doc');
+var environ = require('../../.bem/environ');
 
 
 registry.decl(sourceNodes.SourceNodeName, {
 
     getLibraries : function() {
         return [
-//            'islands-controls',
+            'islands-controls',
+            'islands-media',
+            'islands-page',
             'islands-popups',
-            'islands-user',
-            'islands-media'
+            'islands-search',
+            'islands-services',
+            'islands-user'
         ];
     }
 
