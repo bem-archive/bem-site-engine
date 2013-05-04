b5d4eebeb6d2c3df4a9f0a78e052166379857325
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-05-04 22:34:34 +0400

    Тестовая реализация b-page

--- desktop.blocks/b-page/b-page.deps.js
+++ desktop.blocks/b-page/b-page.deps.js
@@ -4,6 +4,7 @@
         { mods : { theme : 'normal' } },
         { block : 'header' },
         { block : 'layout', mods : { type : 'serp' } },
+        { block : 'layout', mods : { section : '100' } },
         { block : 'content' },
         { block : 'footer' }
     ]
