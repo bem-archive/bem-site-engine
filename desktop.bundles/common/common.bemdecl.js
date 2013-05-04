75f750905e4ffaefc6af6129f77ae07a1a31f435
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-05-04 22:34:08 +0400

    Задекларировать блоки

--- desktop.bundles/common/common.bemdecl.js
+++ desktop.bundles/common/common.bemdecl.js
@@ -1,7 +1,8 @@
 exports.blocks = [
     { block : 'app', mods : { start : 'auto' } },
-    { block : 'page' },
-    { block : 'catalogue' },
+    { block : 'page-index' },
+    { block : 'page-library' },
+    { block : 'page-catalogue' },
     { block : 'i-global' },
     { block : 'b-page' }
 ];
