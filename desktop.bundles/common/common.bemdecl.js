e5622578426f892263779d9b3cb2ee40890cbf8f
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-05-06 13:39:07 +0400

    page-catalogue renamed to page-document

--- desktop.bundles/common/common.bemdecl.js
+++ desktop.bundles/common/common.bemdecl.js
@@ -2,7 +2,7 @@ exports.blocks = [
     { block : 'app', mods : { start : 'auto' } },
     { block : 'page-index' },
     { block : 'page-library' },
-    { block : 'page-catalogue' },
+    { block : 'page-document' },
     { block : 'i-global' },
     { block : 'b-page' }
 ];
