77c19ed2931f31bd69f0ae340928a93f8edc77ee
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-05-10 04:17:48 +0400

    Блок для отображения документа

---
+++ desktop.blocks/document/document.deps.js
@@ -0,0 +1,9 @@
+({
+    mustDeps : [
+        { block : 'i-bem', elems : ['html', 'tree'] },
+        { block : 'island' }
+    ],
+    shouldDeps : [
+        { block  : 'bk-declaration' }
+    ]
+})
