576fdeffba8657c78f8b26d920afccc77ee5e96d
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-05-10 04:18:13 +0400

    bemhtml для блока content

---
+++ desktop.blocks/content/content.deps.js
@@ -0,0 +1,10 @@
+({
+    mustDeps : [
+        { block : 'i-bem', elems : ['html', 'tree'] }
+    ],
+    shouldDeps : [
+        { block : 'step-history' },
+        { block : 'catalogue' },
+        { block : 'document' }
+    ]
+})
