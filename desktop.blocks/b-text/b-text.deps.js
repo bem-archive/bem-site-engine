8a55f22f92a484eb4c72331db9e9f00e0c3f2145
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-05-16 21:14:55 +0400

    Шаблон для конвертации b-text в нормальные блоки текста из islands.blocks

---
+++ desktop.blocks/b-text/b-text.deps.js
@@ -0,0 +1,11 @@
+({
+    mustDeps : [
+        { block : 'i-bem', elems : ['tree'] },
+        { block : 'static-text' },
+        { block : 'para' },
+        { block : 'link' },
+        { block : 'ulist' },
+        { block : 'olist' },
+        { block : 'headline' }
+    ]
+})
