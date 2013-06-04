371040adbdb8b6ac456cd31c3a047ef9de72a273
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-06-04 15:44:38 +0400

    Отображать исходный BEMJSON-примера по нажатию на кнопку "BEMJSON"

--- desktop.blocks/bk-example/__source/bk-example__source.deps.js
+++ desktop.blocks/bk-example/__source/bk-example__source.deps.js
@@ -1,9 +1,16 @@
 ({
-    mustDeps : [],
+    mustDeps : [
+        { block : 'i-bem', elems : ['dom', 'html'] }
+    ],
     shouldDeps : [
         {
             block : 'check-button',
             mods : { size : 's', pseudo : 'yes' }
-        }
+        },
+        {
+            block : 'i-request',
+            mods : { type : 'ajax' }
+        },
+        { block : 'slide' }
     ]
 })
