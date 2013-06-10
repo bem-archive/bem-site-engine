26f1251a883a43b68943ac232c122bedc20cc16d
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-06-10 04:58:41 +0400

    Используем domain-версию yana-http

--- common.blocks/app/app.deps.js
+++ common.blocks/app/app.deps.js
@@ -3,7 +3,7 @@
         { block : 'modules' }
     ],
     shouldDeps : [
-        { block : 'yana-http' },
+        { block : 'yana-http', mods : { domain : 'yes' } },
         { block : 'yana-handler', mods : { 'type' : 'common' } },
         { block : 'yana-cluster' },
         { elem : 'config' }
