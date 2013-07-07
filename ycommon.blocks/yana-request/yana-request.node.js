19154f8fc1fc9b09762be94db12455237db15636
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-07-07 13:39:27 +0300

    Исправил блоки request/response в соответствии с изменениями в bem-yana#arch-review

--- ycommon.blocks/yana-request/yana-request.node.js
+++ ycommon.blocks/yana-request/yana-request.node.js
@@ -3,30 +3,15 @@
 
 modules.define(
     'yana-request',
-    ['inherit', 'yana-logger', 'yana-config'],
-    function(provide, inherit, logger, config, Request) {
+    ['yana-logger', 'yana-config'],
+    function(provide, logger, config, Request) {
 
-provide(inherit(Request, {
-
-    __constructor : function() {
-        return this.__base.apply(this, arguments)
-            .then(function(req) {
-                Object.defineProperty(req, 'userip', {
-                    get : function() {
-                        return this.headers['x-real-ip'] || this.connection.remoteAddress;
-                    }
-                });
-
-                req.logger = logger;
-
-                req.env = {
-                    config : config
-                };
-
-                return req;
-            });
+Object.defineProperty(Request, 'userip', {
+    get : function() {
+        return this.headers['x-real-ip'] || this.connection.remoteAddress;
     }
+});
 
-}));
+provide(Request);
 
 });
