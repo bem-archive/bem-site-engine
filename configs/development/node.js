a3d212cc3005ad51993b0420d475a8a03e348009
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-07-09 17:58:20 +0300

    Исправил опечатку в названии создаваемого сокета

--- configs/development/node.js
+++ configs/development/node.js
@@ -2,7 +2,7 @@ var OS = require('os'),
     PATH = require('path'),
     appRoot = PATH.resolve(__dirname, '../../'),
     // @type {String} <user>-<app>-www.sock
-    socket = [process.env.USER, PATH.basename(appRoot), '-www.sock'].join('-');
+    socket = [process.env.USER, PATH.basename(appRoot), 'www.sock'].join('-');
 
 module.exports = {
     debug : true,
