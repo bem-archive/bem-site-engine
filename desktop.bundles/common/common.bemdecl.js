7edf671ac4505aa7e1492e0a48c58cf644da7ba2
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-07-26 13:44:40 +0400

    Добавил page-internal-error в bemdecl-бандла

--- desktop.bundles/common/common.bemdecl.js
+++ desktop.bundles/common/common.bemdecl.js
@@ -4,6 +4,7 @@ exports.blocks = [
     { block : 'page-libraries' },
     { block : 'page-block' },
     { block : 'page-not-found' },
+    { block : 'page-internal-error' },
     { block : 'error' },
     { block : 'i-global' },
     { block : 'b-page' }
