6cc25b513922bce3c2c21f7d056452af341a955d
Bemer <bemer@yandex-team.ru>
2013-08-06 22:11:40 +0300

    Исправлена ошибка с отображением иконки библиотеки

--- desktop.blocks/topic/topic.deps.js
+++ desktop.blocks/topic/topic.deps.js
@@ -7,7 +7,7 @@
         { block : 'check-button' },
         { elem : 'header' },
         { elem : 'readme', mods : { visible : 'yes' } },
-        { elem : 'icon', mods : { size : ['library', 'block'] }},
+        { elem : 'icon', mods : { size : ['library', 'block'], 'no-image' : ['library', 'block'] }},
         { mods : { type: ['library', 'blocks', 'block'], active : 'yes' }}  
     ]
 })
