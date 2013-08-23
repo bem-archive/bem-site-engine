612b1e705e1b48e016d930bdeaa669c164ac3cdf
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-08-23 13:51:52 +0400

    Рефакторинг базового конфига уровней блоков

--- .bem/levels/blocks.js
+++ .bem/levels/blocks.js
@@ -2,33 +2,45 @@ var PATH = require('path'),
     environ = require('bem-environ'),
 
     join = PATH.join,
-    resolve = PATH.resolve.bind(null, __dirname),
 
     LEGO_TECHS = environ.getLibPath('romochka', 'tools/bem/techs/nodejs'),
     BEMCORE_TECHS = environ.getLibPath('bem-core', '.bem/techs');
 
+function resolveTechs(registry, prefix) {
+    return function(name) {
+        return registry[name] = join(prefix, name + '.js');
+    };
+}
+
 exports.getTechs = function() {
     var techs = {
-        'bemdecl.js'    : 'bemdecl.js',
-        'deps.js'       : 'deps.js',
-        'js'            : 'js-i',
-        'css'           : 'css',
-        'ie.css'        : 'ie.css',
-        'ie6.css'       : 'ie6.css',
-        'ie7.css'       : 'ie7.css',
-        'ie8.css'       : 'ie8.css',
-        'ie9.css'       : 'ie9.css'
+        'bemdecl.js' : 'v2/bemdecl.js',
+        'deps.js' : 'v2/deps.js',
+        'js' : 'v2/js-i',
+        'css' : 'v2/css',
+        'ie.css'  : 'v2/ie.css',
+        'ie6.css' : 'v2/ie6.css',
+        'ie7.css' : 'v2/ie7.css',
+        'ie8.css' : 'v2/ie8.css',
+        'ie9.css' : 'v2/ie9.css'
     };
 
-    ['html', 'bemhtml', 'bemtree', 'vanilla.js', 'browser.js', 'node.js'].forEach(function(name) {
-        techs[name] = join(BEMCORE_TECHS, [name, 'js'].join('.'));
-    });
+    [
+        'html',
+        'bemhtml',
+        'bemtree',
+        'vanilla.js',
+        'browser.js',
+        'node.js'
+    ].forEach(resolveTechs(techs, BEMCORE_TECHS));
 
-    ['i18n', 'i18n.keys.js'].forEach(function(name) {
-        techs[name] = join(LEGO_TECHS, [name, 'js'].join('.'));
-    });
+    [
+        'i18n',
+        'i18n.keys.js'
+    ].forEach(resolveTechs(techs, LEGO_TECHS));
 
     return techs;
 };
 
 exports.defaultTechs = ['css', 'vanilla.js', 'deps.js', 'bemhtml', 'bemtree'];
+
