78f2053cdc41c1f6427decf2720faf62c4651299
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-08-26 20:05:13 +0400

    yana-template: Добавил доопределение блока с контекстом проектных шаблонов

---
+++ common.blocks/yana-template/__context/yana-template__context.deps.js
@@ -0,0 +1,9 @@
+({
+    shouldDeps : [
+        { block : 'util' },
+        { block : 'http-request' },
+        { block : 'file-provider' },
+        { block : 'ya-auth' },
+        { block : 'le-datasrc' }
+    ]
+})
