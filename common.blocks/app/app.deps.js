120e7b98fc665c4cb4bc59bb1faef5944e6e69d8
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-08-26 20:04:03 +0400

    app: Исправил deps

--- common.blocks/app/app.deps.js
+++ common.blocks/app/app.deps.js
@@ -1,11 +1,8 @@
 ({
-    noDeps: [
+    noDeps : [
         { block : 'modules' }
     ],
     shouldDeps : [
-        { block : 'yana-http', mods : { domain : 'yes' } },
-        { block : 'yana-handler', mods : { 'type' : 'common' } },
-        { block : 'yana-cluster' },
         { elem : 'config' }
     ]
 })
