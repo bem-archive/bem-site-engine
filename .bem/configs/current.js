c061a9ca0e9d7ec24e70660b8316681e70a53324
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-08-26 20:30:54 +0400

    Попраивил code-style

--- .bem/configs/current.js
+++ .bem/configs/current.js
@@ -1,10 +1,7 @@
-var BEM = require('bem');
-
-//process.env.YENV = 'production';
+var extend = require('bem').util.extend;
 
 module.exports = {
-
-    libraries : BEM.util.extend(require('legoa-repodb'), {
+    libraries : extend(require('legoa-repodb'), {
         'bem-controls' : {
             type : 'git',
             url  : 'git://github.com/ymaps/bem-controls.git'
@@ -14,5 +11,4 @@ module.exports = {
             url  : 'git://github.yandex-team.ru/maps/islands-controls.git'
         }
     })
-
 };
