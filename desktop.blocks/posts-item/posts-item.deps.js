db2f0a8b2aebe748778ff6cf390ba3484c481add
Bemer <bemer@yandex-team.ru>
2013-09-14 21:17:04 +0300

    Реализовал отображение автора в мета-информации для каждого поста в виде ссылки ведущей на страницу authors

--- desktop.blocks/posts-item/posts-item.deps.js
+++ desktop.blocks/posts-item/posts-item.deps.js
@@ -3,6 +3,7 @@
         { block : 'i-bem', elems : ['i18n'] }
     ],
     shouldDeps: [
+        { block : 'author', mods : { view: ['full', 'menu-item', 'simple'] } },
         { elem : 'title' },
         { elem : 'date' },
         { elem : 'summary' },
