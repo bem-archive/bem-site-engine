46c4fe4ef892938ec8231db91f531f9a59e83120
Bemer <bemer@yandex-team.ru>
2013-09-23 13:44:02 +0300

    Реализована страница с информацией по автору в дизайне. Упразднен блок posts-item. Его функции насебя взял блок post с соответсвующими модификаторами отображения

--- desktop.blocks/posts-item/__thumbnail/posts-item__thumbnail.vanilla.js
+++
@@ -1,6 +0,0 @@
-/*global modules:false */
-
-modules.define('posts-item__thumbnail', function(provide) {
-
-});
-
--- desktop.blocks/posts/__list/posts__list.deps.js
+++ desktop.blocks/posts/__list/posts__list.deps.js
@@ -1,6 +1,6 @@
 ({
     mustDeps: [],
     shouldDeps: [
-        { block : 'posts-item' }
+        { block : 'post' }
     ]
 })
