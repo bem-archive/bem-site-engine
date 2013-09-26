13c2c8f26200f736cd19d206ba2e6d0af46b1e0b
Bemer <bemer@yandex-team.ru>
2013-09-26 17:28:44 +0300

    Реализовано отображение единичного поста для новостей

--- common.blocks/page-news/page-news.node.js
+++ common.blocks/page-news/page-news.node.js
@@ -13,11 +13,52 @@ router.addRoute({
 
 router.addRoute({
     name : 'news',
-    rule : '/news/.*',
+    rule : '/news/{1}',
     data : {
         action : 'le-page'
     }
 });
+
+router.addRoute({
+    name : 'news',
+    rule : '/news/{1}/{2}',
+    data : {
+        action : 'le-page'
+    }
+});
+
+router.addRoute({
+    name : 'news',
+    rule : '/news/{1}/{2}/{3}',
+    data : {
+        action : 'le-page'
+    }
+});
+
+router.addRoute({
+    name : 'news',
+    rule : '/news/{1}/{2}/{3}/{4}',
+    data : {
+        action : 'le-page'
+    }
+});
+
+router.addRoute({
+    name : 'news',
+    rule : '/news/{1}/{2}/{3}/{4}/{5}',
+    data : {
+        action : 'le-page'
+    }
+});
+
+router.addRoute({
+    name : 'news',
+    rule : '/news/{1}/{2}/{3}/{4}/{5}/{6}',
+    data : {
+        action : 'le-page'
+    }
+});
+
 
 provide(View);
 
