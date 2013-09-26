8e9fc08f1cc21b846fda7c1db7eb7d3ea97ee8e4
Bemer <bemer@yandex-team.ru>
2013-09-26 16:36:12 +0300

    Удалены неиспользуемые файлы

--- common.blocks/routes/routes.deps.js
+++
@@ -1,9 +0,0 @@
-// ({
-//     mustDeps : [
-//         { block : 'yana-view' },
-//         { block : 'le-page' }
-//     ],
-//     shouldDeps : [
-//         { block : 'yana-router' }
-//     ]
-// })
