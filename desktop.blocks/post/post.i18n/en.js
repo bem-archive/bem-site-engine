7e095fb2843161239eee1eb3a7fe61f9f35796d9
Bemer <bemer@yandex-team.ru>
2013-10-01 16:19:27 +0300

    Добавил разделение меток для авторов и переводчиков в зависимости от количества авторов и перводчиков соответственно

--- desktop.blocks/post/post.i18n/en.js
+++ desktop.blocks/post/post.i18n/en.js
@@ -1,7 +1,9 @@
 module.exports = {
     "post": {
-        "author": "Author(s)",
-        "translator": "Translator(s)",
+        "author": "Author",
+        "authors": "Authors",
+        "translator": "Translator",
+        "translators": "Translators",
         "createDate": "Create Date",
         "editDate": "Edit Date",
         "tags": "Tags",
