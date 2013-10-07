302cf4e9ebe5c375a5da1ef151e20e93528ea2ad
Bemer <bemer@yandex-team.ru>
2013-10-07 15:32:24 +0300

    Для статей articles реализовано комбинированное меню категорий и постов

--- desktop.blocks/menu/menu.deps.js
+++ desktop.blocks/menu/menu.deps.js
@@ -4,6 +4,6 @@
         { elem: 'item', mods : { type: ['tags', 'authors', 'categories', 'articles'], active: 'yes', complex: 'yes' } },
         { elem: 'title'},
         { elem: 'list'},
-        { mods: { type: ['authors', 'tags', 'categories', 'articles'] } }
+        { mods: { type: ['authors', 'tags', 'categories', 'articles', 'mixed'] } }
     ]
 })
