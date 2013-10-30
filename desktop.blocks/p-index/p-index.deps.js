5b0d0aedbe764adbf6e55a8026c0c2aa6cfd0bd0
Aleksandra Nekhaieva <lesanra@yandex-team.ru>
2013-10-30 16:07:46 +0200

    BEM-1276: trial version of morda

--- desktop.blocks/p-index/p-index.deps.js
+++ desktop.blocks/p-index/p-index.deps.js
@@ -5,6 +5,10 @@
     ],
     shouldDeps: [
         { elem: 'logo', mods: { theme: ['batman', 'breaks', 'captain-america',
-            'generated', 'ironman', 'motion', 'robin', 'scheme', 'spots', 'stripe', 'waves'] } }
+            'generated', 'ironman', 'motion', 'robin', 'scheme', 'spots', 'stripe', 'waves'] } },
+        { elem: 'title'},
+        { elem: 'phrase'},
+        { elem: 'link'},
+        { elem: 'ya'}
     ]
 })
