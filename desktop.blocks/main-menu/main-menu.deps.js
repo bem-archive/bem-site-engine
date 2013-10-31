d245a6a404a629013bd67b72cd73cf1efeb62c26
Bemer <bemer@yandex-team.ru>
2013-10-31 15:53:12 +0200

    Реализовал скрытие пункта меню вакансии для английской локали. Добавил модификатор hidden который может выставлятьсямодой hidden блока main-menu если пункт меню попадает в карту скрытых пунктов для выбранной локали

--- desktop.blocks/main-menu/main-menu.deps.js
+++ desktop.blocks/main-menu/main-menu.deps.js
@@ -3,8 +3,6 @@
     shouldDeps: [
         { block: 'logobem' },
         { block: 'search' },
-        { elem: 'item', mods: { size: ['small', 'normal'], type: 'delimeter', active: 'yes' } }
-        //{ elem: 'item', mods: { active: 'yes' } },
-        //{ elem: 'item', mods: { type: 'delimeter' } }
+        { elem: 'item', mods: { size: ['small', 'normal'], type: 'delimeter', active: 'yes', hidden: 'yes' } }
     ]
 })
