bc1d4a4a5153d9e9aa4c924f67ddd29d3c487e86
blond <blond@yandex-team.ru>
2013-12-02 13:51:06 +0400

    Added `router` module

---
+++ src/router.js
@@ -0,0 +1,44 @@
+var Susanin = require('susanin'),
+    router = Susanin();
+
+router.addRoute({
+    name: 'index',
+    pattern: '/'
+});
+
+router.addRoute({
+    name: 'method',
+    pattern: '/method(/<id>)(/)'
+});
+
+router.addRoute({
+    name: 'tools',
+    pattern: '/tools(/<category>)(/<sub-category>)(/<id>)(/)'
+});
+
+router.addRoute({
+    name: 'libs',
+    pattern: '/libs(/<lib>)(/<version>)(/<category>)(/<id>)(/)'
+});
+
+router.addRoute({
+    name: 'articles',
+    pattern: '/articles(/<id>)(/)'
+});
+
+router.addRoute({
+    name: 'news',
+    pattern: '/news(/<id>)(/)'
+});
+
+router.addRoute({
+    name: 'tags',
+    pattern: '/tags(/<id>)(/)'
+});
+
+router.addRoute({
+    name: 'authors',
+    pattern: '/authors(/<id>)(/)'
+});
+
+module.exports = router;
