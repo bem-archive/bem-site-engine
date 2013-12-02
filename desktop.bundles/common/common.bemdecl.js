e0eb1d594372d34bd37fa3857ba61639e516fe7d
blond <blond@yandex-team.ru>
2013-12-02 13:42:43 +0400

    Cleared decl of bundles

--- desktop.bundles/common/common.bemdecl.js
+++ desktop.bundles/common/common.bemdecl.js
@@ -1,22 +1,4 @@
 exports.blocks = [
-    { block : 'app', mods : { start : 'auto' } },
-
-    //utility pages
-    { block : 'page-not-found' },
-    { block : 'page-internal-error' },
-    { block : 'page-ping' },
-
-    //user pages
-    { block : 'page-index' },
-    { block : 'page-method' },
-    { block : 'page-articles' },
-    { block : 'page-tools' },
-    { block : 'page-news' },
-    { block : 'page-tags' },
-    { block : 'page-authors' },
-    { block : 'page-libraries' },
-    { block : 'page-common' },
-
     //common blocks
     { block : 'error' },
     { block : 'i-global' },
