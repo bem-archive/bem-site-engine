0cf015c165f81ff81cedd26bcc0b2dba62c9dd7b
blond <blond@yandex-team.ru>
2013-12-04 15:10:33 +0400

    Removed redundant hosts from configs

--- configs/development/hosts.js
+++ configs/development/hosts.js
@@ -2,16 +2,6 @@ var PATH = require('path'),
     appRoot = PATH.resolve(__dirname, '../../');
 
 module.exports = {
-    blackbox : {
-        host : 'http://blackbox.yandex-team.ru',
-        domain : 'yandex-team.ru'
-    },
-    passport : {
-        host : 'http://passport.yandex-team.ru'
-    },
-    center : {
-        host : 'http://center.yandex-team.ru'
-    },
     static : {
         host : ''
     },
--- configs/local/hosts.js
+++ configs/local/hosts.js
@@ -2,16 +2,6 @@ var PATH = require('path'),
     appRoot = PATH.resolve(__dirname, '../../');
 
 module.exports = {
-    blackbox : {
-        host : '',
-        domain : 'yandex-team.ru'
-    },
-    passport : {
-        host : 'http://passport.yandex-team.ru'
-    },
-    center : {
-        host : 'http://center.yandex-team.ru'
-    },
     static : {
         host : 'http://127.0.0.1:8001'
     },
