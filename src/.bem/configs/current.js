14306046bf507b80671aac639b1e84231864eefc
blond <blond@yandex-team.ru>
2013-12-04 13:45:36 +0400

    Added list of libraries to bem configs

--- src/.bem/configs/current.js
+++ src/.bem/configs/current.js
@@ -1,5 +1,26 @@
 var extend = require('bem').util.extend;
 
 module.exports = {
-    libraries : require('legoa-repodb')
+    libraries: {
+        'bem-components': {
+            _id      : 'bem-components',
+            type     : 'git',
+            url      : 'git://github.com/bem/bem-components.git'
+        },
+        'bem-core': {
+            _id      : 'bem-core',
+            type     : 'git',
+            url      : 'git://github.com/bem/bem-core.git'
+        },
+        'bem-techs-core': {
+            _id      : 'bem-techs-core',
+            type     : 'git',
+            url      : 'git://github.com/bem/bem-techs-core.git'
+        },
+        'bem-bl': {
+            _id      : 'bem-bl',
+            type     : 'git',
+            url      : 'git://github.com/bem/bem-bl.git'
+        }
+    }
 };
