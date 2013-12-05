1ae78b49da2e587004cb9155d92e9e45ac6e57af
blond <blond@yandex-team.ru>
2013-12-05 15:15:56 +0400

    Wrapped app logic in `run` function

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ var connect = require('connect'),
     router = require('./router'),
     middleware = require('./middleware');
 
-module.export = connect()
-    .use(connect.logger(config.get('app:logger:mode')))
-    .use(connect.query())
-    .use(middleware.prefLocale(config.get('app:languages'), config.get('app:defaultLanguage')))
-    .use(middleware.router(router))
-    .use(middleware.page())
-    .use(middleware.error())
-    .listen(config.get('app:port'));
+exports.run = function() {
+    connect()
+        .use(connect.logger(config.get('app:logger:mode')))
+        .use(connect.query())
+        .use(middleware.prefLocale(config.get('app:languages'), config.get('app:defaultLanguage')))
+        .use(middleware.router(router))
+        .use(middleware.page())
+        .use(middleware.error())
+        .listen(config.get('app:port'));
+};
