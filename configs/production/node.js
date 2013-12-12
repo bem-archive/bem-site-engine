0d2b063598dcde5694ab13003e15601ae27c0b60
Bemer <bemer@yandex-team.ru>
2013-12-12 17:43:19 +0400

    Change production config. Set workers count to 4

--- configs/production/node.js
+++ configs/production/node.js
@@ -1,13 +1,9 @@
-var OS = require('os'),
-
-    nworkers = OS.cpus().length - 2;
-
 module.exports = {
     debug : false,
     app : {
         env : 'production',
         socket : '/var/run/yandex/bem-info-www/bem-info-www.sock',
-        workers : nworkers < 1? 1 : nworkers
+        workers : 4
     },
     logger : {
         level : 'info',
