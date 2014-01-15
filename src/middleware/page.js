f17d64944897e414bde6b974f3becaeade7eaf28
blond <blond@yandex-team.ru>
2014-01-15 12:52:19 +0400

    Fixed error handling

--- src/middleware/page.js
+++ src/middleware/page.js
@@ -3,8 +3,7 @@ var template = require('../template'),
     BUNDLE_NAME = 'common';
 
 module.exports = function() {
-
-    return function(req, res) {
+    return function(req, res, next) {
         var ctx = {
             req: req,
             bundleName: BUNDLE_NAME,
@@ -14,6 +13,9 @@ module.exports = function() {
         return template.apply(ctx, req.prefLocale, req.query.__mode)
             .then(function(html) {
                 res.end(html);
+            })
+            .fail(function(err) {
+                next(err);
             });
     };
 };
