12047ccab54e9fc8469e5f6e55b0f863ca335c9a
Bemer <bemer@yandex-team.ru>
2014-01-20 12:08:48 +0200

    Implement load and parse methods for sitemap.json file

--- src/le-modules/le-data.js
+++ src/le-modules/le-data.js
@@ -1,10 +1,14 @@
 'use strict';
 
-var API = require('github'),
+var https = require('https'),
+    util = require('util'),
+    path = require('path'),
+
+    api = require('github'),
     vow = require('vow'),
-    HTTPS = require('https'),
-    UTIL = require('util'),
+    fs = require('vow-fs'),
     _ = require('lodash'),
+
     logger = require('../logger')(module),
     config = require('../config'),
 
@@ -20,8 +24,8 @@ exports.init = function() {
     var publicConfig = _.extend(config.get('github:public'), config.get('github:common')),
         privateConfig = _.extend(config.get('github:private'), config.get('github:common'));
 
-    gitPublic = new API(publicConfig);
-    gitPrivate = new API(privateConfig);
+    gitPublic = new api(publicConfig);
+    gitPrivate = new api(privateConfig);
 
     gitPublic.authenticate(config.get('github:auth'));
 
@@ -53,7 +57,7 @@ exports.dropCache = function() {
 exports.getData = function() {
     var _this = this,
         deferred = vow.defer(),
-        url = UTIL.format({
+        url = util.format({
             'public': 'https://raw.github.com/%s/%s/%s/%s',
             'private': 'https://github.yandex-team.ru/%s/%s/raw/%s/%s'
         }[dataRepository.type], dataRepository.user, dataRepository.repo, dataRepository.ref, dataRepository.path);
@@ -62,7 +66,7 @@ exports.getData = function() {
         deferred.resolve(this.getDataFromCache());
     } else {
         logger.debug('load data from: %s', url);
-        HTTPS.get(url, function(res) {
+        https.get(url, function(res) {
             var data = '';
 
             res.on('data', function(chunk) {
@@ -82,3 +86,45 @@ exports.getData = function() {
 
     return deferred.promise();
 };
+
+//----- new beautiful world ------//
+
+var sitemap;
+
+var loadSiteMap = function() {
+    return fs
+        .read(path.join('configs', 'common', 'sitemap.json'), 'utf-8')
+        .fail(
+            function(err) {
+                logger.error('Sitemap loading failed with error %s', err.message);
+            }
+        );
+};
+
+var parseSiteMap = function(data) {
+    var def = vow.defer();
+
+    try {
+        sitemap = JSON.parse(data);
+        def.resolve(sitemap);
+    } catch(err) {
+        logger.error('Sitemap parsed with error %s', err.message);
+        def.reject(sitemap);
+    }
+
+    return def.promise();
+};
+
+var processSiteMap = function(sitemap) {
+    var nodeR = function(item) {
+
+    };
+
+    sitemap.forEach(function(item) {
+        nodeR(item);
+    });
+};
+
+
+
+
