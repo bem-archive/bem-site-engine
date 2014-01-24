f23daff161caac979168c71962da30053b31a1ff
Bemer <bemer@yandex-team.ru>
2014-01-24 12:31:34 +0200

    Refactoring for le-app module. Add js docs for methods

--- src/le-modules/le-app.js
+++ src/le-modules/le-app.js
@@ -26,27 +26,44 @@ module.exports = {
             .then(leData.loadAll)
     },
 
+    /**
+     * Returns array of objects for susanin routes creation
+     * @returns {Array}
+     */
     getRoutes: function() {
         return _.values(routes);
     },
 
+    /**
+     * Returns parsed and post-processed sitemap model
+     * @returns {Object}
+     */
     getSitemap: function() {
         return sitemap;
     }
 };
 
+/**
+ * Load sitemap file from filesystem
+ * @returns {String} - content of sitemap.json file
+ */
 var load = function() {
     logger.debug('Load site map');
 
     return fs
         .read(path.join('configs', 'common', 'sitemap.json'), 'utf-8')
         .fail(
-        function(err) {
-            logger.error('Site map loading failed with error %s', err.message);
-        }
-    );
+            function(err) {
+                logger.error('Site map loading failed with error %s', err.message);
+            }
+        );
 };
 
+/**
+ * Parse content of sitemap json file to js object and handle errors
+ * @param data - content of sitemap.json file
+ * @returns {Object} parsed sitemap object
+ */
 var parse = function(data) {
     logger.debug('Parse site map');
 
@@ -68,18 +85,44 @@ var process = function(sitemap) {
 
     var def = vow.defer(),
         idSourceMap = {},
-        nodeR = function(node, parent) {
 
-            node.id = sha(JSON.stringify(node));
-            node.parent = parent;
+        /**
+         * Creates hash unique id of node -> source
+         * @param node {Object} - single node of sitemap model
+         */
+        processSource = function(node) {
+            if(_.has(node, 'source')) {
+                idSourceMap[node.id] = node.source;
+            }
+        },
 
+        /**
+         * Makes title consistent
+         * @param node {Object} - single node of sitemap model
+         */
+        processTitle = function(node) {
+            if(_.has(node, 'title')) {
+                if(_.isString(node.title)) {
+                    node.title = {
+                        en: node.title,
+                        ru: node.title
+                    }
+                }
+            }
+        },
+
+        /**
+         * Collects routes rules for nodes
+         * @param node {Object} - single node of sitemap model
+         */
+        processRoute = function(node) {
             if(_.has(node, 'route') && _.isObject(node.route)) {
                 var r = node.route;
 
                 if(_.has(r, 'name')) {
                     routes[r.name] = routes[r.name] || { name: r.name, pattern: r.pattern };
                 }else {
-                    r.name = parent.route.name;
+                    r.name = node.parent.route.name;
                 }
 
                 ['defaults', 'conditions', 'data'].forEach(function(item) {
@@ -102,23 +145,23 @@ var process = function(sitemap) {
                 }
 
             }else {
-                node.route = {name: parent.route.name};
+                node.route = {name: node.parent.route.name};
             }
+        },
 
-            //make title consistent
-            if(_.has(node, 'title')) {
-                if(_.isString(node.title)) {
-                    node.title = {
-                        en: node.title,
-                        ru: node.title
-                    }
-                }
-            }
+        /**
+         * Recursive function for traversing tree model
+         * @param node {Object} - single node of sitemap model
+         * @param parent {Object} - parent for current node
+         */
+        nodeR = function(node, parent) {
 
-            //create hash unique id of node -> source
-            if(_.has(node, 'source')) {
-                idSourceMap[node.id] = node.source;
-            }
+            node.id = sha(JSON.stringify(node)); //generate unique id for node as sha sum of node object
+            node.parent = parent; //set parent for current node
+
+            processRoute(node);
+            processTitle(node);
+            processSource(node);
 
             //deep into node items
             if(_.has(node, 'items')) {
