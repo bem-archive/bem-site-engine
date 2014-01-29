b31350c608c7bc6b421fa82c66eba019c9a00c6a
Bemer <bemer@yandex-team.ru>
2014-01-29 16:44:38 +0200

    Implement page with search result for single tag

--- src/cluster/worker.js
+++ src/cluster/worker.js
@@ -1,5 +1,6 @@
 var worker = require('luster'),
     logger = require('./../logger')(module),
+    leData = require('../le-modules').leData,
     app = require('./../app');
 
 if (worker.isWorker) {
