b4a6dd342dad3008906f198c2d42dac29a35f20b
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-02-03 21:26:54 +0200

    removed obsolete code for touch detection from page.js

--- src/blocks/common.blocks/page/page.js
+++ src/blocks/common.blocks/page/page.js
@@ -1,4 +1,3 @@
-/* global DocumentTouch */
 modules.define(
     'i-bem__dom',
     ['jquery', 'dom', 'events'],
@@ -6,33 +5,10 @@ modules.define(
 
 BEMDOM.decl('page', {
 
-    onSetMod: {
-        js: {
-            inited: function() {
-                !(this._isTouch()) && this._resizePostContent()
-                    .bindToWin('resize', this._resizePostContent.bind(this));
-            }
-        }
-    },
     _onClick: function() {
         this.toggleMod('fullscreen');
     },
-    /*
-     * Проверка тач-устройств (при обнаружении добавляется модификатор _touch_yes)
-     * @private
-     * @returns {Boolean}
-     */
-    _isTouch: function() {
-        /* hotfix https://st.yandex-team.ru/BEM-1464
-         * Touch detection idea by Modernizr
-         */
-        if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-            this.setMod('touch', 'yes');
-            return true;
-        }
-        return false;
-    }
-    },{
+    }, {
     live: function() {
         this.liveBindTo('fullscreen', 'click', function() { this._onClick() });
     }
