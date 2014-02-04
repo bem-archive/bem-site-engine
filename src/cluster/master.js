0dba42c9f21674d4f964b3a3170ef07c4fbb0382
Bemer <bemer@yandex-team.ru>
2014-02-04 13:10:35 +0200

    Fix cluster code errors

--- src/cluster/master.js
+++ src/cluster/master.js
@@ -1,24 +1,32 @@
-var master = require('luster'),
-    logger = require('./../logger')(module),
-    config = require('./../config'),
+var fs = require('fs'),
+    master = require('luster'),
+    logger = require('../logger')(module),
+    config = require('../config'),
     socket = config.get('app:socket');
 
 if (master.isMaster) {
+    logger.debug('luster: master process start');
+
     var socket = config.get('app:socket');
 
     if (socket) {
         try {
+            logger.silly('luster: unlink socket');
             fs.unlinkSync(socket);
         } catch (e) {}
     }
 
+    logger.silly('luster: register remote command reload');
     master.registerRemoteCommand('reload', function(sender, value) {
         logger.info('master receive message reload from sender %s with value %s', sender.wid, value);
     });
 
+    logger.silly('luster: register remote command request');
     master.registerRemoteCommand('request', function(sender, value) {
         logger.debug('master receive message request from sender %s with value %s', sender.wid, value);
     });
+
+    logger.debug('luster: master process started');
 }
 
 module.exports = master;
