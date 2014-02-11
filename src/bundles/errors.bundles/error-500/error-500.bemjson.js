c85e5b9bc5b24fee6bb2ab40cd2957d9fd98d7a8
blond <blond@yandex-team.ru>
2014-02-11 12:42:28 +0400

    Fixed error bundles

--- src/bundles/errors.bundles/error-500/error-500.bemjson.js
+++ src/bundles/errors.bundles/error-500/error-500.bemjson.js
@@ -1,20 +1,16 @@
 ({
     "block": "page",
     "title": "Error / BEM",
-    "mods": {
-        "theme": "white",
-        "page": "error"
-    },
     "head": [
         [
             {
                 "elem": "css",
-                "url": "{STATICS_HOST}/bundles/desktop.bundles/common/_common.css",
+                "url": "{STATICS_HOST}/bundles/errors.bundles/error-500/error-500.min.css",
                 "ie": false
             },
             {
                 "elem": "css",
-                "url": "{STATICS_HOST}/bundles/desktop.bundles/common/_common",
+                "url": "{STATICS_HOST}/bundles/errors.bundles/error-500/error-500.min",
                 "ie": true
             }
         ],
@@ -42,202 +38,9 @@
     ],
     "content": [
         {
-            "block": "content",
-            "layout": {
-                "block": "layout",
-                "elem": "col",
-                "mods": {
-                    "type": "center"
-                }
-            },
-            "content": [
-                {
-                    "block": "main-menu",
-                    "content": [
-                        {
-                            "elem": "wrapper",
-                            "content": [
-                                {
-                                    "block": "logobem",
-                                    "url": "http://bem.info"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/method",
-                                    "name": "method"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/tools",
-                                    "name": "tools"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/libs",
-                                    "name": "libs"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "type": "delimeter"
-                                    }
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/articles",
-                                    "name": "articles"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/news",
-                                    "name": "news"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/tags",
-                                    "name": "tags"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/authors",
-                                    "name": "authors"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "normal",
-                                        "active": "",
-                                        "hidden": "yes"
-                                    },
-                                    "url": "/jobs",
-                                    "name": "jobs"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "type": "delimeter"
-                                    }
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "small",
-                                        "active": "",
-                                        "hidden": "yes"
-                                    },
-                                    "url": "http://clubs.ya.ru/bem/",
-                                    "name": "yaru"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "small",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "http://www.facebook.com/#!/groups/209713935765634/",
-                                    "name": "facebook"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "small",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "https://twitter.com/bem_en",
-                                    "name": "twitter"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "small",
-                                        "active": "",
-                                        "hidden": ""
-                                    },
-                                    "url": "/acknowledgement",
-                                    "name": "acknowledgement"
-                                },
-                                {
-                                    "elem": "item",
-                                    "elemMods": {
-                                        "size": "small",
-                                        "type": "lang-switch"
-                                    },
-                                    "url": "http://ru.bem.info/404/",
-                                    "name": "lang"
-                                },
-                                {
-                                    "block": "search",
-                                    "action": "http://yandex.com/sitesearch",
-                                    "content": [
-                                        {
-                                            "block": "input",
-                                            "name": "text",
-                                            "val": ""
-                                        },
-                                        {
-                                            "block": "button",
-                                            "type": "submit"
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "block": "p-error",
-                    "mix": [
-                        {
-                            "block": "layout",
-                            "elem": "col",
-                            "elemMods": { type: "center" }
-                        },
-                        {
-                            "block": "layout",
-                            "elem": "page"
-                        }
-                    ],
-                    "code": 500,
-                    "content": 500
-                }
-            ]
+            "block": "error-billboard",
+            "code": 500,
+            "content": 500
         },
         {
             "block": "metrika"
