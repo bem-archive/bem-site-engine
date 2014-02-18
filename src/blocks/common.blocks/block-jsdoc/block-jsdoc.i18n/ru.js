be63c8b93cdec11eb54201ca282835a97e01c8ff
tavria <tavria@yandex-team.ru>
2014-02-18 18:09:55 +0200

    Add i18n for block pages

---
+++ src/blocks/common.blocks/block-jsdoc/block-jsdoc.i18n/ru.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "block-jsdoc": {
+        "description-title": "Описание",
+        "methods-title": "Методы",
+        "params": "Параметры",
+        "returns": "Возвращаемое значение",
+        "events-title": "События"
+    }
+};
---
+++ src/blocks/common.blocks/block/block.i18n/en.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "block": {
+        "static-title": "Block",
+        "docs": "Docs",
+        "jsdoc": "Javasript API",
+        "examples": "Examples"
+    }
+};
