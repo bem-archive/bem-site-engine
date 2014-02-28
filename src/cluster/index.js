d1d0910ed45ce29c2053e554975a3b8ff85eb16a
Bemer <bemer@yandex-team.ru>
2014-02-28 19:24:54 +0200

    Remove master and worker modules from luster directory. Implement data updater

--- src/cluster/index.js
+++ src/cluster/index.js
@@ -1,5 +1,40 @@
-var luster = require('luster'),
-    config = require('../config');
+var fs = require('fs'),
+    luster = require('luster'),
+    logger = require('../logger')(module),
+    data_updater = require('../modules/data_updater'),
+    config = require('../config'),
+    app = require('../app'),
+    socket = config.get('app:socket');
+
+if (luster.isMaster) {
+    logger.debug('luster: master process start');
+
+    var socket = config.get('app:socket');
+
+    if (socket) {
+        try {
+            logger.debug('luster: unlink socket');
+            fs.unlinkSync(socket);
+        } catch (e) {}
+    }
+
+    data_updater.init(luster).start(luster);
+
+    logger.debug('luster: master process started');
+}
+
+if (luster.isWorker) {
+    logger.debug('start worker process %s', luster.id);
+
+    app.run(luster)
+        .then(function() {
+            logger.info('start application for worker with id %s on port %s',
+                luster.id, process.env.port);
+        })
+        .fail(function(err) {
+            logger.error(err);
+        });
+}
 
 luster.configure({
     app: require.resolve('./worker'),
--- src/modules/data_updater/index.js
+++ src/modules/data_updater/index.js
@@ -10,28 +10,67 @@ var u = require('util'),
     config = require('../../config'),
     data = require('../data');
 
-var job;
+var job,
+    marker;
 
 module.exports = {
 
-    init: function(worker) {
-        logger.info('Init data updater for worker %s', worker.wid);
+    init: function(master) {
+        logger.debug('Init data updater for master process');
 
         job = new cronJob({
-            cronTime: '*/5 * * * * *',
+            cronTime: '*/30 * * * * *',
             onTick: function() {
-                checkForUpdate(worker);
+                checkForUpdate(master);
             },
             start: false
         });
+
+        return this;
     },
 
-    start: function() {
-        logger.info('Start data updater');
+    start: function(worker) {
+        logger.info('Start data updater for master process');
         job.start();
+
+        return this;
     }
 };
 
-var checkForUpdate = function(worker) {
-    logger.debug('Check for update for worker %s start', worker.wid);
+var checkForUpdate = function(master) {
+    logger.debug('Check for update for master process start');
+
+    var promise;
+
+    if('production' === process.env.NODE_ENV) {
+        promise = data.common.loadData(data.common.PROVIDER_YANDEX_DISK, {
+            path: config.get('data:marker:disk')
+        }).then(function(content) {
+                return JSON.parse(content);
+            });
+    }else {
+        promise = data.common.loadData(data.common.PROVIDER_FILE, {
+            path: config.get('data:marker:file')
+        });
+    }
+
+    return promise.then(function(content) {
+        if(!content) {
+            return;
+        }
+
+        if(!marker) {
+            marker = content;
+            return;
+        }
+
+        if(marker.sitemap !== content.sitemap ||
+            marker.docs !== content.docs ||
+            marker.libraries !== content.libraries) {
+
+            master.softRestart();
+        }
+
+        marker = content;
+    });
 };
