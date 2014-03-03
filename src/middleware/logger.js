280229e3279e17b1772015abb6e8bc8fbb2fcf2d
Bemer <bemer@yandex-team.ru>
2014-03-03 11:06:25 +0200

    Change log level in logger middleware from info to debug

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,7 +2,7 @@ var logger = require('../logger')(module);
 
 module.exports = function() {
     return function(req, res, next) {
-        logger.info('request method: %s url: %s locale: %s', req.method, req.url, req.prefLocale);
+        logger.debug('request method: %s url: %s locale: %s', req.method, req.url, req.prefLocale);
         next();
     };
 };
