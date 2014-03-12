8ce33bfb0c3076ea6221a9abb0d886eff312fb3b
Bemer <bemer@yandex-team.ru>
2014-03-12 14:32:44 +0200

    Make monitoring request detection more flexible

--- src/middleware/monitoring.js
+++ src/middleware/monitoring.js
@@ -3,14 +3,15 @@ var logger = require('../logger')(module),
 
 module.exports = function() {
 
-    var PATH = config.get('monitoringUrl') || '/monitoring';
+    var REQ = config.get('monitoringUrl') || '/monitoring';
 
     return function(req, res, next) {
-        if(req._parsedUrl.path !== PATH) {
+        if(req._parsedUrl.path.substring(0, REQ.length) !== REQ) {
             return next();
         }
 
         logger.info('Monitoring request has been received');
+
         res.writeHead(200, { 'Content-Type' : 'text/plain' });
         res.end('Pong');
     };
