a6c7fc627ecc17533da8412bd596b729ae950877
Bemer <bemer@yandex-team.ru>
2014-04-01 17:05:08 +0400

    Fix bin/app.js

--- bin/app.js
+++ bin/app.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 
-var path = require('path');
+var path = require('path'),
+    config = require('../src/config');
 
-if ('production' === process.env.NODE_ENV) {
-    require('../src/cluster').run();
-} else {
+if ('development' === config.get('NODE_ENV')) {
     var nodemon = require('nodemon'),
         config = require(path.join(process.cwd(), 'configs', 'current', 'nodemon'));
 
     config.script = require.resolve('../src/app.js'),
 
-    nodemon(config)
-        .on('start', function() {
-            console.log('nodemon started');
-        })
-        .on('crash', function() {
-            console.log('script crashed for some reason');
-        });
+        nodemon(config)
+            .on('start', function() {
+                console.log('nodemon started');
+            })
+            .on('crash', function() {
+                console.log('script crashed for some reason');
+            });
+
+} else {
+    require('../src/cluster').run();
 }
