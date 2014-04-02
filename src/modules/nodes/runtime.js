b0a0596026186dac540e3ee83099d7a078c6ebaf
Bemer <bemer@yandex-team.ru>
2014-04-02 16:43:24 +0400

    Add js docs to src/modules/nodes/runtime.js module

--- src/modules/nodes/runtime.js
+++ src/modules/nodes/runtime.js
@@ -1,5 +1,11 @@
 var BaseNode = require('./base').BaseNode;
 
+/**
+ * Runtime node object wrapper inherited from BaseNode class
+ * @param node - {Object} source node object
+ * @param parent - {Object} parent node object
+ * @constructor
+ */
 var RuntimeNode = function(node, parent) {
     Object.keys(node).forEach(function(key) { this[key] = node[key]; }, this);
     this.setParent(parent);
