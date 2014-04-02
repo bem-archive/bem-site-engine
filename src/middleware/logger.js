6a50a1bb74fa4ea6196d549fa496a9a87dc9c771
Bemer <bemer@yandex-team.ru>
2014-04-02 16:36:14 +0400

    Add js docs for src/middleware/logger.js module

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,5 +1,9 @@
 var logger = require('../logger')(module);
 
+/**
+ * Middleware for logging requests
+ * @returns {Function}
+ */
 module.exports = function() {
     return function(req, res, next) {
         logger.info('request method: %s url: %s locale: %s', req.method, req.url, req.prefLocale);
