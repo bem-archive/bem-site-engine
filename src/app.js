a90acfbb1f9e80522606e64dd32e46f9b0d6adad
Bemer <bemer@yandex-team.ru>
2014-04-25 16:17:57 +0400

    Code refactoring for src/app.js module

--- src/app.js
+++ src/app.js
@@ -2,84 +2,87 @@ var path = require('path'),
     vow = require('vow'),
     fs = require('fs'),
     express = require('express'),
-    slashes = require('connect-slashes'),
     config = require('./config'),
     logger = require('./logger')(module),
     router = require('./router'),
     middleware = require('./middleware'),
-    app = express(),
-    socket = config.get('app:socket'),
-    port = config.get('app:port') || process.env.port || 8080;
+    model = require('./modules').model;
 
-function run(worker) {
+
+exports.run = function(worker) {
     worker = worker || { wid: 0 };
+    return model.init(worker).then(startServer);
+};
 
-    var model = require('./modules').model,
-        deferred = vow.defer();
+/**
+ * Adds middlewares for development environment
+ * @param app - {Object} express application
+ * @returns {Object} app
+ */
+var addDevelopmentMW = function(app) {
+    var enbServer = require('enb/lib/server/server-middleware'),
+        rootPath = process.cwd();
 
-    return model.init(worker)
-        .then(function() {
-            router.init();
+    app
+        .use(enbServer.createMiddleware({ cdir: rootPath }))
+        .use(express.static(rootPath))
+        .use(express.favicon(path.resolve(rootPath, 'www/favicon.ico')));
 
-            if (config.get('NODE_ENV') === 'development') {
-                var enbServer = require('enb/lib/server/server-middleware'),
-                    rootPath = process.cwd();
+    return app;
+};
 
-                app.use(enbServer.createMiddleware({ cdir: rootPath }));
-                app.use(express.static(rootPath));
-                app.use(express.favicon(path.resolve(rootPath, 'www/favicon.ico')));
+/**
+ * Add middlewares which are used for all environments
+ * @param app - {Object} express application
+ * @returns {Object} app
+ */
+var addCommonMW = function(app) {
+    app
+        .use(express.query())
+        .use(middleware.prefLocale(config.get('app:languages'), config.get('app:defaultLanguage')))
+        .use(middleware.logger())
+        .use(middleware.monitoring())
+        .use(middleware.libProxy())
+        .use(middleware.router(router.router))
+        .use(middleware.page())
+        .use(middleware.error());
+
+    return app;
+};
+
+/**
+ * Starts appliction server
+ * @returns {*}
+ */
+var startServer = function() {
+    router.init();
+
+    var def = vow.defer(),
+        app = express(),
+        socket = config.get('app:socket'),
+        port = config.get('app:port') || process.env.port || 8080;
+
+    config.get('NODE_ENV') === 'development' && addDevelopmentMW(app);
+
+    addCommonMW(app);
+
+    app.listen(port || socket, function(err) {
+        if (err) {
+            def.reject(err);
+            return;
+        }
+
+        if (socket) {
+            try {
+                fs.chmod(socket, '0777');
+            } catch(e) {
+                logger.error('Can\'t chmod 0777 to socket');
             }
+        }
 
-            app
-                .use(express.query())
-                .use(middleware.prefLocale(config.get('app:languages'), config.get('app:defaultLanguage')))
-                .use(middleware.logger())
-                .use(middleware.monitoring())
-                .use(middleware.libProxy())
-                .use(middleware.router(router.router))
-                .use(slashes())
-                .use(middleware.page());
+        logger.info('start application on %s %s', socket && 'socket' || port && 'port', socket || port);
+        def.resolve();
+    });
 
-            /*
-            if (config.get('forum')) {
-                var forum = require('bem-forum/src/middleware/forum'),
-                    forumConfig = {
-                        github: {
-                            api: config.get('github:common'),
-                            auth: config.get('github:auth')
-                        },
-                        repo: config.get('forum:repo'),
-                        route: config.get('forum:route')
-                    },
-                    BEMHTML = require(path.join(process.cwd(), 'src', 'bundles', 'desktop.bundles',
-                        'common', 'common.min.template.i18n')).BEMHTML;
-
-                app.use(forum(forumConfig, BEMHTML));
-            }
-            */
-
-            app.use(middleware.error());
-
-            app.listen(port || socket, function(err) {
-                if (err) {
-                    deferred.reject(err);
-                    return;
-                }
-
-                if (socket) {
-                    try {
-                        fs.chmod(socket, '0777');
-                    } catch(e) {
-                        logger.error('Can\'t chmod 0777 to socket');
-                    }
-                }
-
-                logger.info('start application on %s %s', socket && 'socket' || port && 'port', socket || port);
-                deferred.resolve();
-            });
-
-            return deferred.promise();
-        });
-}
-
-exports.run = run;
+    return def.promise();
+};
