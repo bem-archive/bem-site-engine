f592b86b91cb71243777afe69dd1a7d23c38fa53
tavria <tavria@yandex-team.ru>
2014-04-29 18:33:05 +0400

    Support select choose libs for redirect by current key word

--- src/blocks/common.blocks/menu-list/_type/menu-list_type_default.js
+++ src/blocks/common.blocks/menu-list/_type/menu-list_type_default.js
@@ -1,88 +1,88 @@
 modules.define('i-bem__dom', ['jquery'], function(provide, $, BEMDOM) {
 
-BEMDOM.decl({ block: 'menu-list', modName: 'type', modVal: 'default' }, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                var _this = this;
+    BEMDOM.decl({ block: 'menu-list', modName: 'type', modVal: 'default' }, {
+        onSetMod: {
+            js: {
+                inited: function() {
+                    var _this = this;
 
-                _this.__base();
+                    _this.__base();
 
-                _this.setSelectUrl();
+                    _this.setSelectUrl();
 
-                _this.setLinksUrl();
+                    _this.setLinksUrl();
 
-                this.bindTo('select', 'change', function(e) {
-                    var $this = $(e.currentTarget),
-                        url = $this.val(),
+                    this.bindTo('select', 'change', function(e) {
+                        var $this = $(e.currentTarget),
+                            url = $this.val(),
 
                         // TODO: придумать другой способ получать текст ссылки
-                        libname = $this.prev('.menu-list__link').text();
+                            libname = $this.prev('.menu-list__link').text();
 
-                    _this.showLibPage(url);
-                    _this.setStorage(libname, url);
+                        _this.showLibPage(url);
+                        _this.setStorage(libname, url);
+                    });
+                }
+            }
+        },
+
+        setSelectUrl: function() {
+            var isLibsPath = location.pathname.match(/\/libs\//);
+
+            if(isLibsPath) {
+
+                var _this = this,
+                    locPath = location.pathname,
+                    libLocPath = locPath.match(/\/libs\S+?\/[\w.-]+/);
+
+                _this.elem('select').each(function() {
+                    var libname = $(this).prev('.menu-list__link').text(),
+                        storage = _this.getStorage(libname),
+                        defaultVal = $(this).val();
+
+                    if(storage) {
+
+                        if(libLocPath && libLocPath[0].indexOf(libname + '/') !== -1) {
+
+                            // check if href contains key word 'current'
+                            if(locPath.match(/\/current\//)) {
+                                libLocPath[0] = defaultVal;
+                            } else {
+                                $(this).val(libLocPath[0]);
+                            }
+
+                            _this.setStorage(libname, libLocPath[0]);
+
+                        } else {
+                            $(this).val(storage);
+                        }
+                    } else {
+                        _this.setStorage(libname, defaultVal);
+                    }
                 });
             }
-        }
-    },
+        },
 
-    setSelectUrl: function() {
-        var isLibsPath = location.pathname.match(/\/libs\//);
-
-        if(isLibsPath) {
-
+        setLinksUrl: function() {
             var _this = this,
-                locPath = location.pathname,
-                libLocPath = locPath.match(/\/libs\S+?\/[\w.-]+/);
+                links = _this.elem('link', 'type', 'select');
 
-            _this.elem('select').each(function() {
-                var libname = $(this).prev('.menu-list__link').text(),
-                    storage = _this.getStorage(libname),
-                    defaultVal = $(this).val();
+            _this.elem('select').each(function(idx, el) {
+                var link = links.eq(idx);
 
-                if(storage) {
-
-                    if(libLocPath && libLocPath[0].indexOf(libname + '/') !== -1) {
-
-                        // check if href contains key word 'current'
-                        if(locPath.match(/\/current\//)) {
-                            libLocPath[0] = defaultVal;
-                        } else {
-                            $(this).val(libLocPath[0]);
-                        }
-
-                        _this.setStorage(libname, libLocPath[0]);
-
-                    } else {
-                        $(this).val(storage);
-                    }
-                } else {
-                    _this.setStorage(libname, defaultVal);
-                }
+                link.attr('href', $(el).val());
             });
+        },
+
+        showLibPage: function(url) {
+            var loc = window.location;
+
+            loc.origin = loc.origin || loc.protocol + '//' + loc.hostname + (loc.port ? ':' + loc.port: '');
+
+            loc.href = loc.origin + url;
         }
-    },
+    });
 
-    setLinksUrl: function() {
-        var _this = this,
-            links = _this.elem('link', 'type', 'select');
-
-        _this.elem('select').each(function(idx, el) {
-            var link = links.eq(idx);
-
-            link.attr('href', $(el).val());
-        });
-    },
-
-    showLibPage: function(url) {
-        var loc = window.location;
-
-        loc.origin = loc.origin || loc.protocol + '//' + loc.hostname + (loc.port ? ':' + loc.port: '');
-
-        loc.href = loc.origin + url;
-    }
-});
-
-provide(BEMDOM);
+    provide(BEMDOM);
 
 });
