943f2ced5d4ceaba16f4622a5c222519a9917bd7
Bemer <bemer@yandex-team.ru>
2014-05-19 17:43:57 +0400

    Show first post if user opens list of posts

--- src/modules/router.js
+++ src/modules/router.js
@@ -15,12 +15,21 @@ exports.run = function(req, res) {
     url = removeTrailingSlash(url);
 
     return findNode(req, url, function(result) {
-        if(!result.lib || !result.items) return result;
+        if(result.lib && result.items) {
+            url += '/' + (_.isObject(result.items[0].title) ?
+                result.items[0].title[config.get('app:defaultLanguage')] : result.items[0].title);
 
-        url += '/' + (_.isObject(result.items[0].title) ?
-            result.items[0].title[config.get('app:defaultLanguage')] : result.items[0].title);
+            return findNode(req, url, null);
+        }
 
-        return findNode(req, url, null);
+        if(result.VIEW.POSTS === result.view && result.items && result.items.length) {
+            url = result.items.filter(function(item) {
+                return !item.hidden[req.prefLocale];
+            })[0].url;
+            return findNode(req, url, null);
+        }
+
+        return result;
     });
 };
 
