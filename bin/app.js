b2649e20916ac44cd30373997900ff655ffc2158
Bemer <bemer@yandex-team.ru>
2014-05-21 12:23:22 +0400

    Small code refactoring

--- bin/app.js
+++ bin/app.js
@@ -1,12 +1,7 @@
 #!/usr/bin/env node
 
 var config = require('../src/config'),
-    launcher;
-
-if ('development' === config.get('NODE_ENV')) {
-    launcher = require('../src/app.js');
-} else {
-    launcher = require('../src/cluster');
-}
+    launcher = 'development' === config.get('NODE_ENV') ?
+        require('../src/app.js') : require('../src/cluster');
 
 launcher.run();
