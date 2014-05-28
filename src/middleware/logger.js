c178eb7b1bf44ca6257ba1370bd6b7963852138b
Bemer <bemer@yandex-team.ru>
2014-05-28 18:16:57 +0400

    Provide ability to set localized tags for posts

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -6,7 +6,7 @@ var logger = require('../logger')(module);
  */
 module.exports = function() {
     return function(req, res, next) {
-        logger.info('request method: %s url: %s locale: %s', req.method, req.url, req.prefLocale);
+        logger.info('request method: %s url: %s locale: %s', req.method, decodeURIComponent(req.url), req.prefLocale);
         next();
     };
 };
