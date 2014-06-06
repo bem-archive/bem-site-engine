408f8dc573b94f613c5983f61203206277bdf8d6
Bemer <bemer@yandex-team.ru>
2014-06-07 01:36:16 +0400

    Simplify code of src/middleware/search.js module

--- src/middleware/search.js
+++ src/middleware/search.js
@@ -1,23 +1,19 @@
 var path = require('path'),
-    util = require('util'),
     vow = require('vow'),
 
-    u = require('../util'),
+    util = require('../util'),
     config = require('../config'),
     logger = require('../logger')(module),
     provider = require('../modules/providers');
 
 var SEARCH = {
     PING: '/__search/ping',
-    LOAD: {
-        LIBS: '/__search/libs',
-        BLOCKS: '/__search/blocks'
-    }
+    LIBS: '/__search/libs',
+    BLOCKS: '/__search/blocks'
 };
 
 module.exports = function() {
     return function(req, res, next) {
-
         /**
          * Loads files from local filesystem
          * or Yandex Disk depending on environment
@@ -25,22 +21,24 @@ module.exports = function() {
          * @returns {*}
          */
         var load = function(key) {
-                var opts = { path: path.join(config.get('common:model:dir'), u.isDev() ?
-                        '' : config.get('NODE_ENV'), config.get(key)) };
+                var opts = {
+                    path: path.join(config.get('common:model:dir'),
+                        util.isDev() ? '' : config.get('NODE_ENV'), config.get(key))
+                };
 
                 return provider
-                    .load(u.isDev() ? provider.PROVIDER_FILE : provider.PROVIDER_DISK, opts)
+                    .load(util.isDev() ? provider.PROVIDER_FILE : provider.PROVIDER_DISK, opts)
                     .then(function (content) {
                         return res.end(content);
                     });
             },
-            url = req._parsedUrl.path;
+            url = req.path;
 
         if(url.indexOf(SEARCH.PING) > -1) {
             return load('common:model:marker');
-        }else if(url.indexOf(SEARCH.LOAD.LIBS) > -1){
+        }else if(url.indexOf(SEARCH.LIBS) > -1){
             return load('common:model:search:libraries');
-        }else if(url.indexOf(SEARCH.LOAD.BLOCKS) > -1){
+        }else if(url.indexOf(SEARCH.BLOCKS) > -1){
             return load('common:model:search:blocks');
         }else {
             return next();
