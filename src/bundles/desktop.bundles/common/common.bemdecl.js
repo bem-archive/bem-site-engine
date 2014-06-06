30041cef6fdff59e8edf09ecfcfa7994ebcd0296
Bemer <bemer@yandex-team.ru>
2014-06-06 17:32:00 +0400

    Remove bem-forum blocks from bem-engine

--- src/bundles/desktop.bundles/common/common.bemdecl.js
+++ src/bundles/desktop.bundles/common/common.bemdecl.js
@@ -1,7 +1,5 @@
 exports.blocks = [
     { "name": "error" },
     { "name": "i-global" },
-    { "name": "page" },
-    { "name": "issues-page" },
-    { "name": "issue-page" }
+    { "name": "page" }
 ];
