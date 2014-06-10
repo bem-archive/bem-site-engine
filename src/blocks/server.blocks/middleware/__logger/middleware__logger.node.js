8f333e2d184c7656eab1986d42798eedfebdf425
Bemer <bemer@yandex-team.ru>
2014-06-10 18:08:21 +0400

    Add middleware__logger module

---
+++ src/blocks/server.blocks/middleware/__logger/middleware__logger.node.js
@@ -0,0 +1,10 @@
+modules.define('middleware__logger', ['logger'], function(provide, logger) {
+    logger = logger(module);
+
+    provide(function() {
+        return function(req, res, next) {
+            logger.info('request method: %s url: %s locale: %s', req.method, decodeURIComponent(req.url), req.lang);
+            return next();
+        };
+    });
+});
