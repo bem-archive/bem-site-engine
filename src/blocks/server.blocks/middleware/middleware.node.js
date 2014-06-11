d1b1d7eaa99bc1ceab27968648dfe0409abf4057
Bemer <bemer@yandex-team.ru>
2014-06-11 13:52:11 +0400

    Add new dependencies to middleware module

--- src/blocks/server.blocks/middleware/middleware.node.js
+++ src/blocks/server.blocks/middleware/middleware.node.js
@@ -1,9 +1,20 @@
 modules.define('middleware', [
-    'middleware__locale', 'middleware__logger', 'middleware__monitoring', 'middleware__lang-switcher'],
-    function(provide, locale, logger, monitoring, langSwitcher) {
+    'middleware__locale', 'middleware__logger', 'middleware__monitoring',
+        'middleware__proxy-example', 'middleware__proxy-search', 'middleware__page-title',
+        'middleware__page-meta', 'middleware__lang-switcher'],
+    function(provide, locale, logger, monitoring, proxyExample, proxySearch, pageTitle, pageMeta, langSwitcher) {
 
         return provide(function() {
-            return [ locale, logger, monitoring ];
+            return [
+                locale,
+                logger,
+                monitoring,
+                proxyExample,
+                proxySearch,
+                pageTitle,
+                pageMeta,
+                langSwitcher
+            ];
         });
     }
 );
