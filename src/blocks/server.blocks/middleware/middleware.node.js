485648fcbcc598adb51c4242fb6ab3745a3fa618
Bemer <bemer@yandex-team.ru>
2014-06-11 11:58:12 +0400

    Change return type of provide function

--- src/blocks/server.blocks/middleware/middleware.node.js
+++ src/blocks/server.blocks/middleware/middleware.node.js
@@ -2,8 +2,8 @@ modules.define('middleware', [
     'middleware__locale', 'middleware__logger', 'middleware__monitoring', 'middleware__lang-switcher'],
     function(provide, locale, logger, monitoring, langSwitcher) {
 
-        return provide([
-            locale, logger, monitoring
-        ]);
+        return provide(function() {
+            return [ locale, logger, monitoring ];
+        });
     }
 );
