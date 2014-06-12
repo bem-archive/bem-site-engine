c138fa770b67834d896667e07d24b4e41652360a
Bemer <bemer@yandex-team.ru>
2014-06-12 12:16:24 +0400

    Change config key for libraries repo

--- src/blocks/server.blocks/middleware/__proxy-example/middleware__proxy-example.node.js
+++ src/blocks/server.blocks/middleware/__proxy-example/middleware__proxy-example.node.js
@@ -12,7 +12,7 @@ modules.define('middleware__proxy-example', ['config', 'constants', 'logger', 'u
 
         logger = logger(module);
 
-        var libRepo = config.get('github:librariesRepository');
+        var libRepo = config.get('common:github:librariesRepository');
 
         /**
          * Loads sources for url and sent them to response
@@ -93,6 +93,6 @@ modules.define('middleware__proxy-example', ['config', 'constants', 'logger', 'u
 
                 return next();
             };
-        })
+        });
 
     });
