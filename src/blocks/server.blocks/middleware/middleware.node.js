8d864d282c04d400b80af146032bfa0b8dd90154
Bemer <bemer@yandex-team.ru>
2014-06-17 13:48:48 +0400

    Move specific server modules to lego-site level

--- src/blocks/server.blocks/middleware/middleware.node.js
+++ src/blocks/server.blocks/middleware/middleware.node.js
@@ -1,16 +1,14 @@
 modules.define('middleware', [
         'middleware__locale',
         'middleware__logger',
-        'middleware__monitoring',
         'middleware__proxy-example',
-        'middleware__proxy-search',
         'middleware__router',
         'middleware__page-title',
         'middleware__page-meta',
         'middleware__page-menu',
         'middleware__lang-switcher',
         'middleware__page'],
-    function(provide, locale, logger, monitoring, proxyExample, proxySearch, router, pageTitle, pageMeta, pageMenu, langSwitcher, page) {
+    function(provide, locale, logger, proxyExample, router, pageTitle, pageMeta, pageMenu, langSwitcher, page) {
 
         return provide(function() {
             return [
