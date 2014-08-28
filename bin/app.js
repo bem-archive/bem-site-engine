e33f3ab9fd004d6aae053451d1094b8f04137084
Bemer <bemer@yandex-team.ru>
2014-08-28 16:11:27 +0400

    Add verification for existing bundles before launch

--- bin/app.js
+++ bin/app.js
@@ -1,1 +1,13 @@
-require('../src/bundles/desktop.bundles/common/common.node.js');
+var path = require('path'),
+    fs = require('fs');
+
+var p = path.join(process.cwd(), 'src/bundles/desktop.bundles/common/common.node.js');
+fs.exists(p, function(exists) {
+    if(!exists) {
+        console.error('Error! Bundle node.js file is not exist yet');
+        console.error('You must compile bundles before launch application');
+        return;
+    }
+
+    require('../src/bundles/desktop.bundles/common/common.node.js');
+});
