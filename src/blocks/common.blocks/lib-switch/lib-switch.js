814de06533e4a7322be764cd52c06bc2c5e8bdac
tavria <tavria@yandex-team.ru>
2014-09-30 18:25:26 +0400

    Replace select block to the block from bem-components

---
+++ src/blocks/common.blocks/lib-switch/lib-switch.js
@@ -0,0 +1,36 @@
+modules.define('lib-switch', ['i-bem__dom'], function(provide, BEMDOM) {
+
+    'use strict';
+
+    provide(BEMDOM.decl(this.name, {
+        onSetMod: {
+            js: {
+                inited: function() {
+                    this._select = this.findBlockInside('select');
+
+                    this._setLinkVal();
+                    this._select.on('change', this._onChange, this);
+                }
+            }
+        },
+
+        _setLinkVal: function() {
+            this.findBlockInside('link').domElem.attr('href', this._select.getVal());
+
+            return this;
+        },
+
+        _onChange: function() {
+            this._openLibPage(this._select.getVal());
+        },
+
+        _openLibPage: function(url) {
+            var loc = window.location;
+
+            loc.origin = loc.origin || loc.protocol + '//' + loc.hostname + (loc.port ? ':' + loc.port: '');
+            loc.href = loc.origin + url;
+
+            return this;
+        }
+    }))
+});
