ba61ac497e7cc20ba9c2c7fbb52896745faf52e5
tavria <tavria@yandex-team.ru>
2014-11-26 16:24:34 +0300

    Support exceptions for page title

--- src/blocks/server.blocks/middleware/__page-title/middleware__page-title.node.js
+++ src/blocks/server.blocks/middleware/__page-title/middleware__page-title.node.js
@@ -2,29 +2,37 @@ modules.define('middleware__page-title', ['config', 'logger'], function(provide,
 
     logger = logger(module);
 
-    provide(function() {
-        return function(req, res, next) {
-            logger.debug('get title by request %s', req.url);
+    provide({
+        run: function() {
+            var self = this;
 
-            var node = req.__data.node,
-                traverseTreeNodes = function(node) {
-                    node.url && node.title && titles.push(node.title[req.lang]);
-                    node.parent && traverseTreeNodes(node.parent);
-                },
-                titles = [];
+            return function(req, res, next) {
+                logger.debug('get title by request %s', req.url);
 
+                var node = req.__data.node,
+                    titles = [],
+                    traverseTreeNodes = function(node) {
+                        node.url && node.title && titles.push(node.title[req.lang]);
+                        node.parent && traverseTreeNodes(node.parent);
+                    };
+
+                if(self.hasExceptions(req, node)) return next();
+
+                traverseTreeNodes(node);
+
+                //add common suffix from application configuration
+                titles.push(config.get('title')[req.lang]);
+
+                req.__data.title = titles.join(' / ');
+                return next();
+            };
+        },
+
+        hasExceptions: function(req, node) {
             if(req.url === '/') {
                 req.__data.title = node.title[req.lang];
-                return next();
+                return true;
             }
-
-            traverseTreeNodes(node);
-
-            //add common suffix from application configuration
-            titles.push(config.get('title')[req.lang]);
-
-            req.__data.title = titles.join(' / ');
-            return next();
-        };
+        }
     });
 });
