879d1fe14fa57e52f4b2611db1ffd3c08c06e827
Bemer <bemer@yandex-team.ru>
2014-12-01 17:58:56 +0400

    Support gzipped block examples

--- src/blocks/server.blocks/middleware/__proxy-example/middleware__proxy-example.node.js
+++ src/blocks/server.blocks/middleware/__proxy-example/middleware__proxy-example.node.js
@@ -1,10 +1,9 @@
 var u = require('util'),
-    // fs = require('fs'),
     vm = require('vm'),
-    // path = require('path'),
+    zlib = require('zlib'),
+    https = require('https'),
 
     vow = require('vow'),
-    // vowFs = require('vow-fs'),
     request = require('request'),
     sha = require('sha1'),
     html = require('js-beautify').html,
@@ -16,13 +15,128 @@ modules.define('middleware__proxy-example', ['config', 'constants', 'logger', 'u
 
         var libRepo = config.get('github:libraries');
 
-        function loadHtmlCodeOfBlock(req, url, template) {
+        /**
+         * Loads sources for url and sent them to response
+         * @param {String} url of request
+         * @param {String} ref - value of reference (branch or tag)
+         * @param  {Object} req - response object
+         * @param  {Object} res - response object
+         * @returns {*}
+         */
+        var proxyTextFiles = function (url, ref, req, res) {
+            var originUrl = url;
+
+            // set the content-types by mime type
+            res.type(mime.lookup(url));
+            url = u.format(libRepo.pattern, libRepo.user, libRepo.repo, libRepo.ref, url);
+
+            /**
+             * Retrieve gzipped files via request
+             * @param {String} url - request url
+             * @param {Function} callback - callback function
+             */
+            function getGzipped(url, callback) {
+                var buffer = [],
+                    gunzip = zlib.createGunzip(),
+                    resultStream;
+
+                https.get(url, function (response) {
+                    resultStream = response;
+
+                    if (response.headers['content-encoding'] === 'gzip') {
+                        response.pipe(gunzip);
+                        resultStream = gunzip;
+                    }
+
+                    resultStream.on('data', function (data) {
+                        buffer.push(data.toString());
+                    }).on('end', function() {
+                        callback(null, buffer.join(''));
+                    }).on('error', function (err) {
+                        callback(err);
+                    });
+                }).on('error', function (err) {
+                    callback(err);
+                });
+            }
+
+            /**
+             * Error callback function
+             * @param {Error} error - error object
+             */
+            function onError (error) {
+                if (error) {
+                    logger.warn('req to %s failed with err %s', url, error);
+                    res.end('Error while loading example');
+                }
+            }
+
+            /**
+             * Success callback function
+             * @param {Object} response - response object
+             */
+            function onSuccess (response) {
+                if (/\.bemhtml\.js$/.test(url)) {
+                    response = loadCode(req, originUrl, response);
+                }
+                model.putToCache(sha(url), response);
+                res.end(response);
+            }
+
+            function sendRequest () {
+                logger.debug('request to url: %s', url);
+
+                getGzipped(url, function (error, response) {
+                    onError(error);
+                    onSuccess(response);
+                });
+            }
+
+            /*
+             try to load cached source from local filesystem
+             try to load source from github repository if no cached file was found
+             */
+            return model.getFromCache(sha(url)).then(function (response) {
+                return response ? res.end(response) : sendRequest();
+            });
+        };
+
+        provide(function () {
+            var PATTERN = {
+                    EXAMPLE: '/__example',
+                    FREEZE: '/output'
+                },
+                VERSION_REGEXP = /\/v?\d+\.\d+\.\d+\//;
+
+            return function (req, res, next) {
+                var url = req.path;
+
+                if (url.indexOf(PATTERN.EXAMPLE) > -1) {
+                    return proxyTextFiles(url.replace(PATTERN.EXAMPLE, ''),
+                        VERSION_REGEXP.test(url) ? constants.DIRS.TAG : constants.DIRS.BRANCH, req, res);
+                }
+                if (url.indexOf(PATTERN.FREEZE) > -1) {
+                    return proxyImageFiles(url.replace(PATTERN.FREEZE, ''), res);
+                }
+                return next();
+            };
+        });
+
+        /**
+         * Proxy image files from gh
+         * @param {String} url
+         * @param {Object} res - response object
+         */
+        function proxyImageFiles(url, res) {
+            res.type(mime.lookup(url));
+            request.get(u.format(libRepo.pattern, libRepo.user, libRepo.repo, libRepo.ref, url)).pipe(res);
+        }
+
+        function loadCode(req, url, template) {
             var urlRegExp = /^\/(.+)\/(.+)\/(.+)\/(.+)\/(.+)\/(.+)\.bemhtml\.js$/,
                 match = url.match(urlRegExp);
 
-            if (!match) {
-                return null;
-            }
+            if (!match) { return null; }
 
             return model
                 .getNodesByCriteria(function (record) {
@@ -36,26 +150,17 @@ modules.define('middleware__proxy-example', ['config', 'constants', 'logger', 'u
                         match[4] === r.conditions.block;
                 }, true)
                 .then(function (node) {
-                    if (!node) {
-                        return vow.resolve(null);
-                    }
+                    if (!node) { return vow.resolve(null); }
                     return model.getBlocks()[node.source.key];
                 })
                 .then(function (blockData) {
-                    var example,
-                        htmlStr;
-
-                    if (!blockData) {
-                        return vow.resolve(null);
-                    }
-
+                    var example, htmlStr;
+                    if (!blockData) { return vow.resolve(null); }
                     example = blockData.data[req.lang].examples.filter(function (item) {
                         return item.name && match[5] === item.name;
                     })[0];
 
-                    if (!example) {
-                        return null;
-                    }
+                    if (!example) { return null; }
 
                     var bemhtml = {},
                         bemjson = vm.runInNewContext('(' + example.source + ')', {});
@@ -67,85 +172,4 @@ modules.define('middleware__proxy-example', ['config', 'constants', 'logger', 'u
                     return html(htmlStr, { indent_size: 4 });
                 });
         }
-
-        /**
-         * Loads sources for url and sent them to response
-         * @param {String} url of request
-         * @param {String} ref - value of reference (branch or tag)
-         * @param  {Object} req - response
-         * @param  {Object} res - response
-         * @returns {*}
-         */
-        var proxyTextFiles = function (url, ref, req, res) {
-            var originUrl = url;
-
-            // set the content-types by mime type
-            res.type(mime.lookup(url));
-            url = u.format(libRepo.pattern, libRepo.user, libRepo.repo, libRepo.ref, url);
-
-            var sendRequest = function () {
-                    request(url, function (error, response, body) {
-                        if (!error && response.statusCode === 200) {
-                            if (/\.bemhtml\.js$/.test(url)) {
-                                body = loadHtmlCodeOfBlock(req, originUrl, body);
-                            }
-                            model.putToCache(sha(url), body);
-                            res.end(body);
-                        } else {
-                            res.end('Error while loading example');
-                        }
-                    });
-                };
-
-            /*
-             try to load cached source from local filesystem
-             try to load source from github repository if no cached file was found
-             */
-            return model.getFromCache(sha(url))
-                .then(function (value) {
-                    if (value) {
-                        return res.end(value);
-                    }
-                    return sendRequest();
-                });
-        };
-
-    function proxyImageFiles(url, res) {
-        // set the content-types by mime type
-        res.type(mime.lookup(url));
-        url = u.format(libRepo.pattern, libRepo.user, libRepo.repo, libRepo.ref, url);
-        request.get(url).pipe(res);
-
-        // var p = path.resolve(constants.DIRS.CACHE, sha(url));
-        // vowFs.exists(p).then(function (exists) {
-        //    if (exists) {
-        //        fs.createReadStream(p).pipe(res);
-        //    }else {
-        //        var x = request.get(url);
-        //        x.pipe(fs.createWriteStream(p));
-        //        x.pipe(res);
-        //    }
-        // });
-    }
-
-    provide(function () {
-        var PATTERN = {
-                EXAMPLE: '/__example',
-                FREEZE: '/output'
-            },
-            VERSION_REGEXP = /\/v?\d+\.\d+\.\d+\//;
-
-        return function (req, res, next) {
-            var url = req.path;
-
-            if (url.indexOf(PATTERN.EXAMPLE) > -1) {
-                return proxyTextFiles(url.replace(PATTERN.EXAMPLE, ''),
-                    VERSION_REGEXP.test(url) ? constants.DIRS.TAG : constants.DIRS.BRANCH, req, res);
-            }
-            if (url.indexOf(PATTERN.FREEZE) > -1) {
-                return proxyImageFiles(url.replace(PATTERN.FREEZE, ''), res);
-            }
-            return next();
-        };
-    });
 });
