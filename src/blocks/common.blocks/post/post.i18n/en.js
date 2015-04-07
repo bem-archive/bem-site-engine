a10e132a29438d24c6e2faf96bfb8d2cd315d0f8
gela <gela-d@yandex-team.ru>
2015-04-07 11:44:09 +0300

    Pick off links on prose.io

--- src/blocks/common.blocks/post/post.i18n/en.js
+++ src/blocks/common.blocks/post/post.i18n/en.js
@@ -35,11 +35,8 @@ module.exports = {
         "translate": "translate",
 
         "repo-text": "If you've spotted a typo or a mistake, or wish to add something on, you could ",
-        "repo-on-github": " on GitHub",
-        "repo-alternative": ", either ",
-        "repo-with-prose": " with prose.io.",
         "repo-issue": "write about this",
-        "repo-prose": "edit this article",
+        "repo-on-github": " on GitHub.",
 
         "url": "View it on Github",
         "deps": "Dependencies:"
