c8bd1437e939d12b09652b08b72afb8ec963fb1d
tormozz48 <tormozz48@gmail.com>
2015-04-20 19:12:58 +0300

    Move http storage code to lego-site blocks level

--- src/blocks/server.blocks/storage/storage.node.js
+++ src/blocks/server.blocks/storage/storage.node.js
@@ -1,57 +1,25 @@
 var url = require('url'),
     util = require('util'),
     _ = require('lodash'),
-    request = require('request'),
     MDS = require('mds-wrapper');
 
 modules.define('storage', ['logger', 'config'],
     function (provide, logger, config) {
-        logger = logger(module);
-
-
-        var MODES = { MDS: 0, HTTP: 1 },
-            mode = MODES.MDS,
-            mds = new MDS(config.get('mds')),
-            http = {
-                read: function (url, callback) {
-                    var c = config.get('http'),
-                        host = c.host,
-                        port = c.port,
-                        _url = url.format({
-                            protocol: 'https',
-                            hostname: host,
-                            port: port,
-                            pathname: url
-                        });
-
-                    request.get(_url, function (error, response, body) {
-                        error ? callback(error) : callback(null, body);
-                    });
-                },
-
-                getFullUrl: function (url) {
-                    var c = config.get('http'),
-                        host = c.host,
-                        port = c.port;
-                    return util.format('https://%s:%s/%s', host, port, url);
-                }
-            };
+        var mds;
 
         provide({
 
-            // this method should be overrided for using other service
-            init: function() {
-                mode = MODES.MDS;
+            getStorage: function () {
+                mds = mds || new MDS(config.get('mds'));
+                return mds;
             },
 
             read: function (url, callback) {
-                var s = mode === MODES.MDS ? mds : http;
-                s.read(arguments);
+                this.getStorage().read(arguments);
             },
 
             getFullUrl: function (url) {
-                var s = mode === MODES.MDS ? mds : http;
-                return s.getFullUrl(url);
+                return this.getStorage().getFullUrl(url);
             }
         });
     }
