bb5619aa12817cbed51b02741e6f51e0db1b2937
gela <gela-d@yandex-team.ru>
2015-04-21 14:21:42 +0300

    redesign jsdoc

--- src/blocks/common.blocks/block-jsdoc/block-jsdoc.i18n/ru.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "block-jsdoc": {
-        "description-title": "Описание",
-        "methods-title": "Методы",
-        "params": "Параметры",
-        "returns": "Возвращаемое значение",
-        "events-title": "События"
-    }
-};
